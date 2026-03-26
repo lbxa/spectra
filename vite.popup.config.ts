f41a28f63777e8a0658b89b48b6bb26ee490dbc1
Lucas Barbosa <lucas.brsa@hotmail.com>
2026-03-27 10:03:40 +1100

    react compiler

--- vite.popup.config.ts
+++ vite.popup.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()]
+    }),
+    tailwindcss(),
+  ],
   publicDir: "public",
   define: {
     "process.env": {},
